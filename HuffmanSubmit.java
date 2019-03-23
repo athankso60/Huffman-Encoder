54b601655aa78b56bb1ec9b841f22574e1a5eee8
涛 <34599053+yitao-yu@users.noreply.github.com>
2019-03-22 21:29:06 -0400

    Update HuffmanSubmit.java

--- HuffmanSubmit.java
+++ HuffmanSubmit.java
@@ -91,148 +91,137 @@ public class HuffmanSubmit implements Huffman {
    
    /** Huffman tree node implementation: Base class */
    public interface HuffBaseNode<E> {
-   public boolean isLeaf();
-   public int weight();
+	   public boolean isLeaf();
+	   public int weight();
+	   public int frequency();
    }
    
    /** Huffman tree node: Leaf class */
    class HuffLeafNode<E> implements HuffBaseNode<E> {
-   private E element; // Element for this node
-   private int weight; // Weight for this node
-   private int frequency; // this holds the frequency of the char
-   
-   /** Constructor */
-   public HuffLeafNode(E el, int wt, int freq)
-   { element = el; weight = wt; freq = frequency; }
-   /** @return The element value */
-   public E element() { return element; }
-   /** @return The weight */
-   public int weight() { return weight; }
-   /** Return true */
-   public boolean isLeaf() { return true; }
-   
-   /**Return the frequency*/
-   public int frequency() {
-	   return frequency;
-   }
-   
-   public boolean equals(HuffLeafNode node) {
-	if(node.weight == this.weight) {
-		return true;
-	}
-	   return false;
+	   private E element; // Element for this node
+	   private int weight; // Weight for this node
+	   private int frequency; // this holds the frequency of the char
 	   
-   }
+	   /** Constructor */
+	   public HuffLeafNode(E el, int wt, int freq)
+	   { element = el; weight = wt; freq = frequency; }
+	   /** @return The element value */
+	   public E element() { return element; }
+	   /** @return The weight */
+	   public int weight() { return weight; }
+	   /** Return true */
+	   public boolean isLeaf() { return true; }
+	   
+	   /**Return the frequency*/
+	   public int frequency() {
+		   return frequency;
+	   }
+	   
+	   public boolean equals(HuffLeafNode node) {
+		if(node.weight == this.weight) {
+			return true;
+		}
+		   return false;
+	   }
    }
    
    
    /** Huffman tree node: Internal class */
    class HuffInternalNode<E> implements HuffBaseNode<E> {
-   private int weight; // Weight (sum of children)
-   private HuffBaseNode<E> left; // Pointer to left child
-   private HuffBaseNode<E> right; // Pointer to right child
-   /** Constructor */
-   public HuffInternalNode(HuffBaseNode<E> l,
-   HuffBaseNode<E> r, int wt)
-   { left = l; right = r; weight = wt; }
-   /** @return The left child */
-   public HuffBaseNode<E> left() { return left; }
-   /** @return The right child */
-   public HuffBaseNode<E> right() { return right; }
-   /** @return The weight */
-   public int weight() { return weight; }
-   /** Return false */
-   public boolean isLeaf() { return false; }
-   
-   
+	   private int weight; // Weight (sum of children)
+	   private HuffBaseNode<E> left; // Pointer to left child
+	   private HuffBaseNode<E> right; // Pointer to right child
+	   /** Constructor */
+	   public HuffInternalNode(HuffBaseNode<E> l,
+	   HuffBaseNode<E> r, int wt)
+	   { left = l; right = r; weight = wt; }
+	   /** @return The left child */
+	   public HuffBaseNode<E> left() { return left; }
+	   /** @return The right child */
+	   public HuffBaseNode<E> right() { return right; }
+	   /** @return The weight */
+	   public int weight() { return weight; }
+	   
+	   public int frequency() {return -1;}
+	   /** Return false */
+	   public boolean isLeaf() { return false; }
    }
    
-   class tree{
-	   HuffInternalNode<?> access;
-	   
-	   
-	   int index = 3;
-	   HuffInternalNode<?> buildTreeHelper(ArrayList<HuffLeafNode> LeafArray, HuffInternalNode<?> internal){
-		   //end condition
-		   if(LeafArray.get(index+1) == null) {
-			   access = new HuffInternalNode<>(LeafArray.get(index),internal,internal.weight+LeafArray.get(index).frequency());
-			   //reset index
-			   index = 3;
-			   return null;
+   HuffTree<?> buildTreeHelper(ArrayList<HuffTree> TreeArray){
+	   //end condition
+	   if(TreeArray.size()==2) {
+		   HuffTree<Character> tree;
+		   if(TreeArray.get(0).compareTo(TreeArray.get(1))==1) {
+			   tree = new HuffTree<Character>(TreeArray.get(0).root,TreeArray.get(1).root, TreeArray.get(0).weight()+TreeArray.get(1).weight());
 		   }
-		   //recursive steps
-		   HuffInternalNode<?> temp = new HuffInternalNode<>(LeafArray.get(index),internal,internal.weight+LeafArray.get(index).frequency());
-		   index++;
-		   return buildTreeHelper(LeafArray, temp);
+		   else {
+			   tree = new HuffTree<Character>(TreeArray.get(1).root,TreeArray.get(0).root, TreeArray.get(0).weight()+TreeArray.get(1).weight());
+		   }
+		   return tree;
 	   }
 	   
-	   /** Build a Huffman tree from list hufflist */
-	   void buildTree(ArrayList<HuffLeafNode> LeafArray) {
-		    //sort the array!
-		    for(int i = 0; i < LeafArray.size(); i++) {
-		    	for(int j = 0; j < LeafArray.size()-i; j++) {
-		    		if(LeafArray.get(j).frequency > LeafArray.get(j+1).frequency) {
-		    			swap(LeafArray.get(j),LeafArray.get(j+1));
-		    		}
-		    	}
-		    }
-		    
-		    HuffInternalNode<?> temp = new HuffInternalNode<>(LeafArray.get(2), LeafArray.get(1), LeafArray.get(2).frequency+LeafArray.get(1).frequency);
-		    buildTreeHelper(LeafArray,temp);
-	//   HuffTree tmp1, tmp2, tmp3 = null;
-	//   while (Hheap.size > 1) { // While two items left
-	//   tmp1 = Hheap.extractMax();
-	//   tmp2 = Hheap.extractMax();
-	//   tmp3 = new HuffTree<Character>(tmp1.root(), tmp2.root(),
-	//   tmp1.weight() + tmp2.weight());
-	//   Hheap.insert(tmp3); // Return new tree to heap
-	//   }
-	
-	//   return tmp3; // Return the tree
+	   //recursive steps
+	   HuffTree<Character> tempTree;
+	   if(TreeArray.get(0).compareTo(TreeArray.get(1))==1) {
+		   tempTree = new HuffTree<Character>(TreeArray.get(0).root,TreeArray.get(1).root, TreeArray.get(0).weight()+TreeArray.get(1).weight());
+	   }
+	   else {
+		   tempTree = new HuffTree<Character>(TreeArray.get(1).root,TreeArray.get(0).root, TreeArray.get(0).weight()+TreeArray.get(1).weight());
+	   }
+	   TreeArray.remove(1);
+	   TreeArray.remove(0);
+	   int i;
+	   for(i = 0; i<TreeArray.size(); i++) {
+		   if(tempTree.compareTo(TreeArray.get(i))==1){
+			   TreeArray.add(i-1, tempTree);
+			   break;
+		   }
+	   }
+	   return buildTreeHelper(TreeArray);
+   }
+   
+   public HuffTree<?> HuffmanSubmit(ArrayList<HuffLeafNode> LeafArray){
+	   ArrayList<HuffTree> TreeArray = new ArrayList<HuffTree>();
+	   for(HuffLeafNode N:LeafArray) {
+		   HuffTree<?> temptree = new HuffTree<>(N.element,N.frequency);
+		   TreeArray.add(temptree);
 	   }
 	   
-	   
-	   public String search(char x, int frequency) {
-		   
-		   //for search we need to go left if 0 and right if 1
-		   
-		   
-		   
-		   return null;
-	   }
-	   
-	   public void searchHelper(HuffInternalNode node, Character x, int frequency) {
-		   
-		   
-		   
-	   }
-	   
+	   HuffTree<?> tree = buildTreeHelper(TreeArray);
+	   return tree;
    }
    
    
    /** A Huffman coding tree */
    class HuffTree<E> implements Comparable<HuffTree<E>>{
-   private HuffBaseNode<E> root; // Root of the tree
-   /** Constructors */
-   public HuffTree(E el, int wt){ 
-	   root = new HuffLeafNode<E>(el, wt,0); //we used zero subject to change
+	   private HuffBaseNode<E> root; // Root of the tree
+	   /** Constructors */
+	   public HuffTree(E el, int frequency){ 
+		   root = new HuffLeafNode<E>(el, 0,frequency); //we used zero subject to change
+		   }
+	   
+	   public HuffTree(HuffBaseNode<E> l,HuffBaseNode<E> r, int wt){
+		   root = new HuffInternalNode<E>(l, r, wt); 
+		  }
+
+	public HuffBaseNode<E> root() { 
+	   			return root; 
 	   }
-   
-   public HuffTree(HuffBaseNode<E> l,HuffBaseNode<E> r, int wt){
-	   root = new HuffInternalNode<E>(l, r, wt); 
-	  }
-   
-   public HuffBaseNode<E> root() { 
-   			return root; 
-   }
-   public int weight() // Weight of tree is weight of root
-   { return root.weight(); }
-   
-   public int compareTo(HuffTree<E> that) {
-   if (root.weight() < that.weight()) return -1;
-   else if (root.weight() == that.weight()) return 0;
-   else return 1;
-   }
+	   public int weight() // Weight of tree is weight of root
+	   { 
+		   if(!this.root.isLeaf()) {
+			   return root.weight(); 
+		   }
+		   else {
+			   return root.frequency();
+		   }
+	   }
+	   
+	   public int compareTo(HuffTree<E> that) {
+	   if (root.weight() < that.weight()) return -1;
+	   else if (root.weight() == that.weight()) return 0;
+	   else return 1;
+	   }
    }
    
    public void swap(HuffLeafNode<String> a, HuffLeafNode<String> b) {
